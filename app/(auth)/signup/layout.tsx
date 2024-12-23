f1f30dcbb41688e5e0c0afdffc0b70442f6e0fc5
Anubhav Singh <anubhavsinghbkj@gmail.com>
2024-12-24 03:31:35 +0530

    13.1 complete

---
+++ app/(auth)/signup/layout.tsx
@@ -0,0 +1,8 @@
+export default function Signup({ children }){
+    return <div className="border-b text-center">
+        20% off the next three month 
+    </div>
+    
+    {children}
+    {}
+}
---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,8 @@
+export default function Signup() {
+    return (
+      <div> 
+        Hello Anubhav ! from root file
+      </div>
+    );
+  }
+  
