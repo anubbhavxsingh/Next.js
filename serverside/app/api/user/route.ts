98acf591efe10280624d2a2dbd8a55087937c204
Anubhav Singh <anubhavsinghbkj@gmail.com>
2024-12-26 00:03:45 +0530

    file/folder structure

---
+++ serverside/app/api/user/route.ts
@@ -0,0 +1,16 @@
+export function GET(){
+    // db logic
+    return Response.json({
+        email: "anubhavsinghbkj@gmail.com",
+        name: "Anubhav"
+    })
+}
+export function POST(){
+    // db logic
+    return Response.json({
+        email: "anubhavsinghbkj@gmail.com",
+        name: "Anubhav"
+    })
+}
+
+// check in postman
